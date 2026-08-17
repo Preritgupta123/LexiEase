0cd82c92851cc2f0d402390bc38409d7eb78fd59
prerit1234 <preritgupta560@gmail.com>
2026-08-17 15:39:47 +0530

    fix: show landing page at root route instead of login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import SignupPage from './pages/SignupPage'
 import LoginPage from './pages/LoginPage'
 import DashboardPage from './pages/DashboardPage'
@@ -8,12 +8,14 @@ import LandingPage from './pages/LandingPage'
 function App() {
   return (
     <Routes>
-      {/* Public routes */}
+      {/* Landing page - first thing users see */}
       <Route path="/" element={<LandingPage />} />
+
+      {/* Auth routes */}
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
 
-      {/* Protected route: only accessible when logged in */}
+      {/* Protected route - only when logged in */}
       <Route
         path="/dashboard"
         element={
@@ -22,6 +24,9 @@ function App() {
           </ProtectedRoute>
         }
       />
+
+      {/* Catch all - redirect unknown routes to home */}
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   )
 }
